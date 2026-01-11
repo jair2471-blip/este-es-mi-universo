b75be4f3ef1d90f778c71da725c3ca447bc9d60e
Jair <jair2471@gmail.com>
2026-01-11 00:13:43 -0500

    correccion menu

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,24 +1,43 @@
+import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
+
 export const headerData = {
   links: [
-    { text: 'Inicio', href: getPermalink('/') },
-    { text: 'Bitácora', href: getBlogPermalink() },
-    { text: 'Antonov', href: getPermalink('/antonov') },
-    // Agrupamos las herramientas aquí:
+    {
+      text: 'Inicio',
+      href: getPermalink('/'),
+    },
+    {
+      text: 'Bitácora',
+      href: getBlogPermalink(),
+    },
+    {
+      text: 'Antonov',
+      href: getPermalink('/antonov'),
+    },
     {
       text: 'Herramientas',
       links: [
-        { text: '⏳ Contador de Vida', href: getPermalink('/contadordevida') },
-        { text: '🔥 Compromisos', href: getPermalink('/generador-compromisos') },
+        {
+          text: 'Contador de Vida',
+          href: getPermalink('/contadordevida'),
+        },
+        {
+          text: 'Compromisos',
+          href: getPermalink('/generador-compromisos'),
+        },
       ],
     },
-    { text: 'El manifiesto', href: getPermalink('/elmanifiesto') },
+    {
+      text: 'El manifiesto',
+      href: getPermalink('/elmanifiesto'),
+    },
   ],
-  actions: [], 
+  actions: [],
 };
 
 export const footerData = {
   links: [],
   secondaryLinks: [],
-  socialLinks: [], 
+  socialLinks: [],
   footNote: `© 2026 E.U.L.M · El Universo de la Mente`,
 };
