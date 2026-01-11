f38ac503d32254eb59b19f4a2413b8234d7143ec
Jair <jair2471@gmail.com>
2026-01-10 23:34:44 -0500

    correcion menu

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,10 +1,11 @@
-import { getPermalink, getBlogPermalink, getAsset } from './utils/permalink
+import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
+
 export const headerData = {
   links: [
     { text: 'Inicio', href: getPermalink('/') },
     { text: 'Bitácora', href: getBlogPermalink() },
     { text: 'Antonov', href: getPermalink('/antonov') },
-    { text: 'Contador de Vida', href: getPermalink('/contadordevida') }, // <-- ¡Misión cumplida!
+    { text: 'Contador de Vida', href: getPermalink('/contadordevida') },
     { text: 'El manifiesto', href: getPermalink('/elmanifiesto') },
   ],
   actions: [], 
