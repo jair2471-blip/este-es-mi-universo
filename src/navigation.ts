9c04fbc605704193c570300b89a7b9cabf8e554f
Jair <jair2471@gmail.com>
2026-01-13 09:18:16 -0500

    que si que

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,5 +1,7 @@
 import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 
+import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
+
 export const headerData = {
   links: [
     {
@@ -25,6 +27,10 @@ export const headerData = {
           text: 'Compromisos',
           href: getPermalink('/generador-compromisos'),
         },
+        {
+          text: 'Oráculo de Hierro',
+          href: getPermalink('/oraculo'),
+        },
       ],
     },
     {
@@ -33,11 +39,4 @@ export const headerData = {
     },
   ],
   actions: [],
-};
-
-export const footerData = {
-  links: [],
-  secondaryLinks: [],
-  socialLinks: [],
-  footNote: `© 2026 E.U.L.M · El Universo de la Mente`,
 };
