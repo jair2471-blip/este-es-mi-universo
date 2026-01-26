fb5d3a197b3ec36ff64ee3e277972c16b76831ad
Jair <jair2471@gmail.com>
2026-01-26 16:26:02 -0500

    Simplificando config para evitar archivos faltantes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
-import { readingTimeRemark } from './src/utils/frontmatter.mjs';
 
 export default defineConfig({
+  // Solo lo esencial para que funcione React y el diseño
   integrations: [
     react(),
     tailwind({ applyBaseStyles: false }),
-    sitemap(),
   ],
-  markdown: {
-    remarkPlugins: [readingTimeRemark],
-  },
   vite: {
     resolve: {
       alias: {
