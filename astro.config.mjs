11176ca9c82275a8140705491a8b85ae97b8a7ce
Jair <jair2471@gmail.com>
2026-01-28 13:01:35 -0500

    siote

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://eluniversodelamente.com',
+  output: 'static',
   base: '/',
   integrations: [
     react(),
