c97c0303bd354df814bdf924c88bc568efd1cad4
jair2471-blip <jair2471@gmail.com>
2026-02-07 12:56:54 -0500

    Update PortalCosmico.jsx

--- src/components/PortalCosmico.jsx
+++ src/components/PortalCosmico.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'; // Quitamos useRef
-import { Sparkles, Orbit, Rocket, Star, Atom, Moon, Sun, Brain, X } from 'lucide-react'; 
+import { Orbit, Rocket, Star, Atom, Moon, Sun, Brain, X } from 'lucide-react';
 // Quitamos Zap, Globe, Radio, Send, Download que no se usaban
 
 const SABIDURIA_COSMICA = {
